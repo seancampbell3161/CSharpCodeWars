3e3eda4c375601b060d924474c5b3c72b744ea40
Sean Campbell <sean.campbell3161@gmail.com>
2021-08-22 13:42:16 -0500

    find divisible numbers - check whether string indexes are equal

---
+++ beastFeast/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace beastFeast
+{
+    class Program
+    {
+
+        public static bool Feast(string beast, string dish)
+        {
+            return beast[0] == dish[0] && beast[beast.Length - 1] == dish[dish.Length - 1];
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Feast("elephant", "eggplant"));
+            System.Console.WriteLine(Feast("cobra", "cake"));
+        }
+    }
+}
---
+++ divisibleNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace divisibleNumbers
+{
+    class Program
+    {
+
+        public static int[] DivisibleBy(int[] numbers, int divisor)
+        {
+            List<int> resultList = new List<int>();           
+            foreach (int num in numbers)
+            {
+            if (num % divisor == 0)
+                resultList.Add(num);
+            }
+            return resultList.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] result = DivisibleBy(new int[] {1,2,3,4,5,6}, 2);
+            foreach(int i in result)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
